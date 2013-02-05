bd7207158735f8b582ac0c3089151ba9a26ccb83
Justin Wozniak <wozniak@mcs.anl.gov>
2013-02-05 19:54:18 +0000

    Drop debugging
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/release/0.1.0@6148 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/util/debug-tokens.tcl
+++ code/src/util/debug-tokens.tcl
@@ -12,14 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
+# DEBUG-TOKENS.TCL
 # This file is expected to be regularly edited to control
 # debugging output.  It is sourced by debug-auto.tcl
 # The variable INPUT is converted to an array by debug-auto.tcl
 # Each token maps to ON or OFF to indicate debugging
 
 set INPUT {
-    TURBINE ON
-    TCL_TURBINE ON
-    ADLB ON
-    CACHE ON
+    TURBINE OFF
+    TCL_TURBINE OFF
+    ADLB OFF
+    CACHE OFF
 }
