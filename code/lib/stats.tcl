a3eae33f8f68727f48f6b071fd5dfc1d8991ed3c
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-04-14 19:31:05 +0000

    Parallel reduction for stat combine.  Add extra test.  Test not working yet.
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/branches/issue-586-engine@12143 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/lib/stats.tcl
+++ code/lib/stats.tcl
@@ -232,68 +232,132 @@ namespace eval turbine {
     }
 
 
-    # take a container of PartialStats and summarize them
-    # outputs are mean, stddev
-    # note: we return population std dev
-    proc stat_combine { outputs container } {
-      set n_out [ lindex $outputs 0 ]
-      set mean_out [ lindex $outputs 1 ]
-      set std_out [ lindex $outputs 2 ]
-      # TODO: revert to non-deep rule once we change back container repr
-      deeprule $container [ list 1 ] [ list ref ] \
-          "stat_combine_body $container $n_out $mean_out $std_out" \
-          name "stats-combine-$container" 
+  # take a container of PartialStats and summarize them
+  # outputs are mean, stddev
+  # note: we return population std dev
+  proc stat_combine { outputs container } {
+    set n_out [ lindex $outputs 0 ]
+    set mean_out [ lindex $outputs 1 ]
+    set std_out [ lindex $outputs 2 ]
+    rule $container "stat_combine_body $container $n_out $mean_out $std_out" \
+        name "stats-combine-$container" 
+  }
+  
+  # Setup reduction tree to calculate statistics
+  # container: closed container
+  proc stat_combine_body { container n_out mean_out std_out } {
+
+    # TODO: don't really need keys, but enumerate doesn't support
+    # acquiring references
+    set contents [ adlb::acquire_ref $container container 1 1 ]
+    set structs [ dict values $contents ]
+    set n [ llength $structs ]
+    
+    if { $n == 0 } {
+      error "mean and standard deviation not defined for sample size 0"
+    }
+    
+    # Set up a k-way reduction tree
+    set k 4
+
+    # Number of internal nodes in reduce tree
+    set reduce_node_count 0
+    set level_nodes $n
+    while { $level_nodes > $k } {
+      # ceil(level_nodes / k)
+      set level_nodes [ expr {($level_nodes - 1) / $k + 1} ]
+      incr reduce_node_count $level_nodes
     }
 
-    proc stat_combine_body { container n_out mean_out std_out } {
-      set CHUNK_SIZE 65536
-      set mean_accum 0.0
-      set M2_accum 0.0
-      set n_accum 0
-      set members [ adlb::enumerate $container members all 0 ]
-
-
-      foreach struct_td $members {
-        set struct [ retrieve_struct $struct_td ]
-        set n [ dict get $struct "n" ]
-        set mean [ dict get $struct "mean" ]
-        set M2 [ dict get $struct "M2" ]
-        if { $n_accum == 0 } {
-          # Initialize to first value
-          set n_accum $n
-          set mean_accum $mean
-          set M2_accum $M2
-          set first_elem 0
-        } else {
-          # combine statistics
-          set mean2 $mean_accum
-          set n2 $n_accum
-          # n' := n1 + n2
-          set n_accum [ expr {$n_accum + $n} ]
-
-          # weighted mean
-          # mean' := (mean1 * n1 + mean2 * n2) / (n1 + n2)
-          set mean_accum [ expr ( $mean2 * $n2 + $mean * $n) / \
-                                            double( $n_accum ) ]
-
-          #  diff := mean2 - mean1
-          set diff [ expr {$mean - $mean2} ]
-
-          # M2' := M2_1 + M2_2 + diff^2 * ( n1*n2 / (n1 + n2))
-          set M2_accum [ expr $M2_accum + $M2 + \
-                        (($diff**2) * ($n2 * $n / double($n_accum))) ]
-        }
-      }
-
-      # TODO: change back for inline structs in array
-      read_refcount_decr $container
-      if { $n == 0 } {
-        # TODO: change back for inline structs in array
-        # read_refcount_decr $container
-        error "mean and standard deviation not defined for sample size 0"
-      }
-      store_integer $n_out $n_accum
-      store_float $mean_out $mean_accum
-      store_float $std_out [ expr {sqrt($M2_accum / (double($n_accum)))} ]
+    # Allocate intermediate nodes
+    if { $reduce_node_count > 0 } {
+      set node_create_spec [ list s:PartialStats 1 ]
+      set node_create_specs [ lrepeat $reduce_node_count $node_create_spec ]
+      set reduce_nodes [ adlb::multicreate {*}$node_create_specs ]
     }
+   
+    # Setup intermediate reduction
+    set curr_structs $structs
+    # Index into allocated reduce nodes
+    set reduce_node_ix 0
+    while { [ llength $curr_structs ] > $k } {
+      set next_structs [ list ]
+
+      set n [ llength $curr_structs ]
+      for {set i 0} {$i < $n} {incr i $k} {
+        # Slice of structs to reduce
+        set slice [ lrange $curr_structs $i [ expr {$i + $k - 1} ]]
+
+        # Output of reduction
+        set output [ lindex $reduce_nodes $reduce_node_ix ]
+        incr reduce_node_ix
+        rule $slice [ list stat_combine_reduce $slice $output ]
+        lappend next_structs $output
+      }
+
+      set curr_structs $next_structs
+    }
+    
+    # Produce final output
+    rule $curr_structs [ list stat_combine_final \
+      [ lindex [ dict values $contents ] ] $n_out $mean_out $std_out ]
+
+  }
+  
+  proc stat_combine_step { partials n_var mean_var M2_var } {
+    upvar 1 $n_var n_accum
+    upvar 1 $mean_var mean_accum
+    upvar 1 $M2_var M2_accum
+    set n_accum 0
+
+    foreach struct_td $partials { 
+      set struct [ retrieve_decr_struct $struct_td ]
+      set n [ dict get $struct "n" ]
+      set mean [ dict get $struct "mean" ]
+      set M2 [ dict get $struct "M2" ]
+
+      if { $n_accum == 0 } {
+        set n_accum $n
+        set mean_accum $mean
+        set M2_accum $M2
+      } else {
+        # combine statistics
+        set mean2 $mean_accum
+        set n2 $n_accum
+        # n' := n1 + n2
+        set n_accum [ expr {$n_accum + $n} ]
+
+        # weighted mean
+        # mean' := (mean1 * n1 + mean2 * n2) / (n1 + n2)
+        set mean_accum [ expr ( $mean2 * $n2 + $mean * $n) / \
+                                          double( $n_accum ) ]
+
+        #  diff := mean2 - mean1
+        set diff [ expr {$mean - $mean2} ]
+
+        # M2' := M2_1 + M2_2 + diff^2 * ( n1*n2 / (n1 + n2))
+        set M2_accum [ expr $M2_accum + $M2 + \
+                      (($diff**2) * ($n2 * $n / double($n_accum))) ]
+      }
+    }
+  }
+  
+  # Intermediate reduction
+  proc stat_combine_reduce { partials out } {
+    #puts "REDUCE: $partials => $out"
+    stat_combine_step $partials n mean M2
+    set out_val [ dict create n $n mean $mean M2 $M2 ]
+    store_struct $out $out_val s:PartialStats
+  }
+
+  # Final step of reduction: assign output variables
+  proc stat_combine_final { partials n_out mean_out std_out } {
+    #puts "REDUCE FINAL: $partials => $n_out $mean_out $std_out"
+    stat_combine_step $partials n mean M2
+
+    store_integer $n_out $n
+    store_float $mean_out $mean
+    store_float $std_out [ expr {sqrt($M2 / (double($n)))} ]
+  }
 }
+
