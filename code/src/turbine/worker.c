699404943d98c7e89135c782d0d21542246e9868
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-06-26 16:00:42 +0000

    Revert worker.c to trunk version for plain CPU worker
    git-svn-id: https://svn.mcs.anl.gov/repos/exm/sfw/turbine/branches/issue-503@13201 dc4e9af1-7f46-4ead-bba6-71afc04862de

--- code/src/turbine/worker.c
+++ code/src/turbine/worker.c
@@ -45,94 +45,52 @@ turbine_code
 turbine_worker_loop(Tcl_Interp* interp, void* buffer, size_t buffer_size,
                     int work_type)
 {
-    int rc;
-    int max_slots = 50;
-    int current_slots = 1; // Must be >0 and < max_slots
-    Tcl_Obj* active_slots;
-    int adlb_first_get_flag = 1;
+  int rc;
+
+  service_init();
+
+  while (true)
+  {
     MPI_Comm task_comm;
-
-    service_init();
-
-    while (true)
+    int work_len, answer_rank, type_recved;
+    adlb_code code = ADLB_Get(work_type, buffer, &work_len,
+                              &answer_rank, &type_recved, &task_comm);
+    if (code == ADLB_SHUTDOWN)
+      break;
+    turbine_task_comm = task_comm;
+    if (code != ADLB_SUCCESS)
     {
-        int work_len, answer_rank, type_recved;
-        adlb_code code;
-
-        // Get work if there are available slots
-        if ( current_slots < max_slots )
-        {
-            // The first request for work should be a blocking ADLB_Get
-            if ( adlb_first_get_flag == 1 )
-            {
-                printf("First pass - Using ADLB_Get \n");
-                code = ADLB_Get(work_type, buffer, &work_len,
-                                &answer_rank, &type_recved, &task_comm);
-                turbine_task_comm = task_comm;
-                adlb_first_get_flag = 0;
-            // Subsequent work requests are non-blocking Igets
-            } else {
-                printf("Using ADLB_Iget \n");
-                code = ADLB_Iget(work_type, buffer, &work_len,
-                                 &answer_rank, &type_recved);
-            }
-    
-            // Got work
-            if ( code == ADLB_SUCCESS )
-            {
-                assert(work_len <= buffer_size);
-                assert(type_recved == work_type);
-                
-                // Work unit is prepended with rule ID, followed by space.
-                char* rule_id_end = strchr(buffer, ' ');
-                assert(rule_id_end != NULL);
-                // Set pointer to start of Tcl work unit string
-                char * command = rule_id_end + 1;
-
-                DEBUG_TURBINE("rule_id: %"PRId64"", atol(buffer));
-                DEBUG_TURBINE("eval: %s", command);
-                // Work out length | null byte | prefix
-                int cmd_len = work_len - 1 - (int)(command - (char*) buffer);
-                rc = Tcl_EvalEx(interp, command, cmd_len, 0);
-                if (rc != TCL_OK)
-                {
-                    task_error(interp, rc, command);
-                    return TURBINE_ERROR_EXTERNAL;
-                }
-            }
-            // No work
-            if ( code == ADLB_SHUTDOWN && current_slots == 0)
-            {
-                printf("ADLB_SHUTDOWN called with no active slots");
-                break;
-            }
-            if ( code == ADLB_NOTHING && current_slots == 0)
-            {
-                printf("ADLB_NOTHING returned : No More work! \n");
-                break;
-            }
-        }
-        // If we reach here, there is some job in the slots
-        if ( current_slots > 0 )
-        {
-            rc = Tcl_EvalEx(interp, "turbine::poll_mock", 19 ,0 );
-            if (rc != TCL_OK)
-            {
-                // Not sure what to report as task_error here.
-                // Command may not match with actual error
-                //task_error(interp, rc, command);
-                return TURBINE_ERROR_EXTERNAL;
-            } else {
-                active_slots = Tcl_GetObjResult(interp);
-                Tcl_GetIntFromObj(interp, active_slots, &current_slots);
-            }
-            printf("Current_slots = %d \n", current_slots);
-            // TODO : Fix this , sleep in code is a bad idea.
-            usleep(50000); // Sleep for 50ms
-        }
+      printf("Get failed with code %i\n", code);
+      return TURBINE_ERROR_ADLB;
     }
-    service_finalize();
-    return TURBINE_SUCCESS;
+    assert(work_len <= buffer_size);
+    assert(type_recved == work_type);
+
+    // Work unit is prepended with rule ID, followed by space.
+    //char* rule_id_end = strchr(buffer, ' ');
+    //assert(rule_id_end != NULL);
+    // Set pointer to start of Tcl work unit string
+    //char* command = rule_id_end + 1;
+
+    // DEBUG_TURBINE("rule_id: %"PRId64"", atol(buffer));
+
+    // Don't set rule_id
+    char *command = buffer;
+    DEBUG_TURBINE("eval: %s", command);
+
+    // Work out length | null byte | prefix
+    int cmd_len = work_len - 1 - (int)(command - (char*) buffer);
+    rc = Tcl_EvalEx(interp, command, cmd_len, 0);
+    if (rc != TCL_OK)
+    {
+      task_error(interp, rc, command);
+      return TURBINE_ERROR_EXTERNAL;
+    }
+  }
+
+  service_finalize();
+
+  return TURBINE_SUCCESS;
 }
 
 static void
